bef9da3e62042479a6e523c90ed427a4d857fc7c
Sushma <sushmat@menlo-technologies.com>
2020-04-09 14:58:40 +0530

    OMNG-827 - #1 point - Fixed

--- src/features/submission/nodeSequenceTree.js
+++ src/features/submission/nodeSequenceTree.js
@@ -61,15 +61,23 @@ class NodeSequenceTree extends Component {
     //based on DTD versions 2.01 and 3.3
     const dtd_version = getDTDVersion(m1Json);
     const label =
-      dtd_version == "2.01"
-        ? sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${
-            sequence.submission_type
-          }) ${getV2_2Date(m1Json)}` : `${submissionLabel}\\${sequence.name}`
-        : sequence.submission_sub_type
+      // dtd_version == "2.01"
+      //   ? sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${
+      //       sequence.submission_type
+      //     }) ${getV2_2Date(m1Json)}` : `${submissionLabel}\\${sequence.name}`
+      //   : sequence.submission_sub_type
+      //   ? sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${sequence.submission_type}-${sequence.submission_sub_type})`
+      //   : `${submissionLabel}\\${sequence.name}`
+      //   : sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${sequence.submission_type})` :
+      //   `${submissionLabel}\\${sequence.name}`;
+
+        sequence.submission_sub_type
         ? sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${sequence.submission_type}-${sequence.submission_sub_type})`
         : `${submissionLabel}\\${sequence.name}`
-        : sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${sequence.submission_type})` :
-        `${submissionLabel}\\${sequence.name}`;
+        : sequence.submission_type ? `${submissionLabel}\\${sequence.name} (${sequence.submission_type}) ${getV2_2Date(m1Json)}` :
+        `${submissionLabel}\\${sequence.name} ${getV2_2Date(m1Json)}`;
+
+
     return label;
   };
 
